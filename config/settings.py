a0bf1b6478714864a67f07591762e5ddfb29591b
Tamir Mandreev <tamirmandreev@mail.ru>
2025-06-05 04:42:28 +0800

    Установил Django и DRF

--- config/settings.py
+++ config/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+# Список INSTALLED_APPS определяет, какие приложения Django будут активны в проекте
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -37,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
+    'rest_framework', # Подключить Django REST Framework
 ]
 
 MIDDLEWARE = [
