40170b31a546b486f839ade2fe633c88e16bb718
Tamir Mandreev <tamirmandreev@mail.ru>
2025-06-06 03:11:15 +0800

    Создал кастомный менеджер модели
    При выполнении команды python3 manage.py createsuperuser возникала ошибка TypeError: UserManager.create_superuser() missing 1 required positional argument: 'username'. В кастомной модели пользователя аргумент 'username' не требуется. Вместо него используется 'email'. В связи с этим был создан кастомный менеджер модели

--- users/models.py
+++ users/models.py
@@ -1,6 +1,53 @@
 from django.db import models
 # AbstractUser - абстрактный класс, который включает все стандартные поля Django-модели User
-from django.contrib.auth.models import AbstractUser
+# BaseUserManager - это класс, который предоставляет основные методы для создания и управления пользователями
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+# Создать кастомный менеджер для модели User
+class UserManager(BaseUserManager):
+    # Включить кастомный менеджер модели в миграции базы данных
+    use_in_migrations = True
+
+    # Создать приватный метод для создания пользователя
+    # Метод не предназначен для прямого вызова извне - только внутри менеджера
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('Пользователи должны иметь email')
+        # Привести домен email к нижнему регистру
+        email = self.normalize_email(email)
+        # Создать объект пользователя
+        user = self.model(email=email, **extra_fields)
+        # Установить пароль
+        user.set_password(password)
+        # Сохранить в базу данных
+        user.save(using=self._db)
+        # Вернуть созданный и сохраненный объект пользователя (для дальнейшего использования)
+        return user
+
+    # Создать публичный интерфейс для создания обычного (не административного) пользователя
+    def create_user(self, email, password=None, **extra_fields):
+        # У пользователя по умолчанию нет прав суперпользователя
+        extra_fields.setdefault('is_superuser', False)
+        # У пользователя по умолчанию нет доступа к административной панели Django
+        extra_fields.setdefault('is_staff', False)
+        # Создать пользователя через приватный метод
+        return self._create_user(email, password, **extra_fields)
+
+    # Создать публичный интерфейс для создания суперпользователя
+    def create_superuser(self, email, password, **extra_fields):
+        # У пользователя по умолчанию прав суперпользователя
+        extra_fields.setdefault('is_superuser', True)
+        # У пользователя по умолчанию есть доступ к административной панели Django
+        extra_fields.setdefault('is_staff', True)
+
+        # Добавить проверки. Защита от явной передачи False
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Суперпользователь должен иметь is_superuser=True.')
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Суперпользователь должен иметь is_staff=True.')
+
+        # Создать пользователя через приватный метод
+        return self._create_user(email, password, **extra_fields)
 
 # Создать кастомную модель пользователя
 class User(AbstractUser):
@@ -12,6 +59,10 @@ class User(AbstractUser):
     USERNAME_FIELD = "email"
     # Определить список полей, которые будут запрашиваться при создании пользователя через команду createsuperuser
     REQUIRED_FIELDS = []
+
+    # Создать и назначить менеджер модели
+    # Он отвечает за все взаимодействия с базой данных для этой модели
+    objects = UserManager()
 
     # Определить строкое представление пользователя
     def __str__(self):
