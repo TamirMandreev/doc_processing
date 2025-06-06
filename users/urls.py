914c96beae4855e8e7dfd735602a632067d0e6d6
Tamir Mandreev <tamirmandreev@mail.ru>
2025-06-07 01:52:22 +0800

    Создал маршруты для получения access и refresh токенов т

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
+# TokenObtainPairView - представление для получения пары токенов (access и refresh)
+# TokenRefreshView - представление для обновления access токена с помощью refresh токена
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 from users.apps import UsersConfig
 from users.views import UserCreateAPIView
 
@@ -8,4 +12,7 @@ app_name = UsersConfig.name # Пространство имен приложен
 
 urlpatterns = [
     path('register/', UserCreateAPIView.as_view(), name='register'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
