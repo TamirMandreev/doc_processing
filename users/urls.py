fd9217e4fb8e2f5a42d1600998169250e541c99b
Tamir Mandreev <tamirmandreev@mail.ru>
2025-06-06 21:20:43 +0800

    Создал маршрут в users/urls.py для регистрации пользователя

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
 from users.apps import UsersConfig
+from users.views import UserCreateAPIView
 
 
 app_name = UsersConfig.name # Пространство имен приложения
 
-urlpatterns = []
+urlpatterns = [
+    path('register/', UserCreateAPIView.as_view(), name='register'),
+]
