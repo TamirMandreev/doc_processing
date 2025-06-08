68ff2e01e3dbab9897981ef0e6b2b953ba1a9a93
Tamir Mandreev <tamirmandreev@mail.ru>
2025-06-09 04:40:02 +0800

    Создал маршрут для DocumentCreateAPIView

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls', namespace='users')),
+    path('users/', include('users.urls', namespace='users')), # Подключить маршруты из приложения users
+    path('documents/', include('documents.urls', namespace='documents')), # Подключить маршруты из приложения documents
 ]
---
+++ documents/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from documents.apps import DocumentsConfig
+from documents.views import DocumentCreateAPIView
+
+app_name = DocumentsConfig.name # Пространство имен приложения
+
+urlpatterns = [
+    path('upload/', DocumentCreateAPIView.as_view(), name='document-upload'),
+]
