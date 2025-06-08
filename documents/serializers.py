24b85fac1bad15ed71e2a321946c94c4dece35b3
Tamir Mandreev <tamirmandreev@mail.ru>
2025-06-09 04:31:42 +0800

    Создал сериализатор для модели Document

---
+++ documents/serializers.py
@@ -0,0 +1,10 @@
+# ModelSerializer предназначен для упрощения процесса преобразования моделей Django в формат JSON и обратно
+from rest_framework.serializers import ModelSerializer
+
+from documents.models import Document
+
+
+class DocumentSerializer(ModelSerializer):
+    class Meta:
+        model = Document
+        fields = '__all__'
