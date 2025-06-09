1d6f0c37f20eb56c5d52b5f2263d1a52d91cd857
Tamir Mandreev <tamirmandreev@mail.ru>
2025-06-10 01:38:21 +0800

    Зарегистрировал модель User в панели администратора

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import User
+
+# Зарегистрировать модель User в панели администратора
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    # Отобразить в списке пользователей следующие поля
+    list_display = ('email',)
