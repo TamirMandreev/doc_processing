e9483ab80a889e8d4077c2c293c23852a6f3b29f
Tamir Mandreev <tamirmandreev@mail.ru>
2025-06-11 03:01:50 +0800

    Настроил Celery

--- config/settings.py
+++ config/settings.py
@@ -161,3 +161,14 @@ SIMPLE_JWT = {
 MEDIA_URL = '/media/'
 # Абсолютный путь к директории на сервере, где Django будет сохранять загруженные пользователями файлы
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Использовать часой пояс Django-проекта
+CELERY_TIMEZONE = TIME_ZONE
+# Фиксировать время начала задачи
+CELERY_TASK_TRACK_STARTED = True
+# Автоматически прерывать задачи, выполняющиеся более 30 минут
+CELERY_TASK_TIME_LIMIT = 30 * 60
+# URL Redis-сервера, который Celery будет использовать как очередь задач
+CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL')
+# URL Redis-сервера для хранения результатов выполненных задач
+CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND')
