084e6b71b3e20c4b10b84684578010b4943e54de
Tamir Mandreev <tamirmandreev@mail.ru>
2025-06-12 05:03:09 +0800

    Зарегистрировал модель Document в панели администратора

--- documents/admin.py
+++ documents/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from documents.models import Document
+
+# Зарегистрировать модель Document в панели администратора
+@admin.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+    # Отобразить в списке пользователей следующие поля
+    list_display = ('title',)
