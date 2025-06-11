1babbbd1e747f1f6d0515fb0fabe388403660cf4
Tamir Mandreev <tamirmandreev@mail.ru>
2025-06-12 04:30:42 +0800

    Создал тест кастомной команды create_adminuser

--- users/tests/test_create_adminuser.py
+++ users/tests/test_create_adminuser.py
@@ -9,7 +9,15 @@ from users.models import User
 
 
 @pytest.mark.django_db # Разрешить доступ к базе данных для этого теста
-def test_create_adminuser():
+def test_create_adminuser(capsys):
+    '''
+    Тестирует создание администратора
+    :param capsys: Фикустура, которая перехватывает все, что программа выводит в консоль
+    :return:
+    '''
+    # Убедиться, что админ не существует
+    User.objects.filter(email=settings.ADMIN_EMAIL).delete()
+
     # Вызвать команду
     call_command('create_adminuser')
 
@@ -17,4 +25,30 @@ def test_create_adminuser():
     admin = User.objects.get(email=settings.ADMIN_EMAIL)
     assert admin.is_staff is True
     assert admin.is_active is True
+
+    captured = capsys.readouterr()
+    assert f'Администратор {settings.ADMIN_EMAIL} создан' in captured.out
+
+
+@pytest.mark.django_db # Разрешить доступ к базе данных для этого теста
+def test_create_adminuser_when_admin_exists(capsys):
+    '''
+    Тестирует создание администратора, когда администратор уже есть
+    :param capsys: Фикустура, которая перехватывает все, что программа выводит в консоль
+    :return:
+    '''
+
+    # Создать администратора
+    User.objects.create(
+        email=settings.ADMIN_EMAIL,
+        password=settings.ADMIN_PASSWORD,
+        is_staff=True,
+    )
+    # Вызвать команду
+    call_command('create_adminuser')
+
+    # Перехватить вывод
+    captured = capsys.readouterr()
+    assert f'Администратор {settings.ADMIN_EMAIL} уже существует!' in captured.out
+
 
