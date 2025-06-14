0ab17a39009b91ceb9ec20a761f98b31605160a9
Tamir Mandreev <tamirmandreev@mail.ru>
2025-06-15 02:57:07 +0800

    Закончил тестировать основной функционал программы

--- users/tests/test_create_adminuser.py
+++ users/tests/test_create_adminuser.py
@@ -5,7 +5,22 @@ from django.core.management import call_command
 # Импортировать настройки проекта
 from django.conf import settings
 
-from users.models import User
+from users.models import User, UserManager
+from users.tests.test_user_model import superuser
+
+
+@pytest.fixture
+def user_manager():
+    '''
+    Создает менеджер для модели User
+    :return:
+    '''
+    # Создать экземпляр менеджера
+    manager = UserManager()
+    # Установить модель, с которой будет работать менеджер
+    manager.model = User
+    # Вернуть менеджер
+    return manager
 
 
 @pytest.mark.django_db # Разрешить доступ к базе данных для этого теста
@@ -50,5 +65,46 @@ def test_create_adminuser_when_admin_exists(capsys):
     # Перехватить вывод
     captured = capsys.readouterr()
     assert f'Администратор {settings.ADMIN_EMAIL} уже существует!' in captured.out
+
+
+@pytest.mark.django_db # Разрешить доступ к базе данных для этого теста
+def test_create_user_without_email(user_manager):
+    '''
+    Тестирует создание пользователя без email
+    :param user_manager: менеджер модели User
+    :return:
+    '''
+
+    # В следующем блоке кода должно произойти исключение ValueError
+    with pytest.raises(ValueError) as excinfo:
+        user_manager.create_user(email=None)
+
+    assert 'Пользователи должны иметь email' in str(excinfo.value)
+
+
+@pytest.mark.django_db # Разрешить доступ к базе данных для этого теста
+def test_create_superuser_without_staff_flag(user_manager):
+    '''
+    Тестирует создание суперпользователя без указания is_superuser=True
+    :return:
+    '''
+
+    with pytest.raises(ValueError) as excinfo:
+        user_manager.create_superuser(email='superuser@example.com', password=12345,is_staff=False)
+
+    assert 'Суперпользователь должен иметь is_staff=True' in str(excinfo.value)
+
+
+@pytest.mark.django_db # Разрешить доступ к базе данных для этого теста
+def test_create_superuser_without_superuser_flag(user_manager):
+    '''
+    Тестирует создание суперпользователя без указания is_superuser=True, is_staff=True
+    :return:
+    '''
+
+    with pytest.raises(ValueError) as excinfo:
+        user_manager.create_superuser(email='superuser@example.com', password=12345,is_superuser=False)
+
+    assert 'Суперпользователь должен иметь is_superuser=True' in str(excinfo.value)
 
 
